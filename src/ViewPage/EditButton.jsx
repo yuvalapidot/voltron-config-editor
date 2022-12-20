2600c37064fa6042c1cd99eff318009106836667
Dor <dorber1@gmail.com>
2022-12-20 18:30:27 +0200

    edit button works

---
+++ src/ViewPage/EditButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@mui/material";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+
+let editorIsClosed = true;
+
+function EditButton() {}
+
+export default EditButton;
--- src/ViewPage/ViewPage.jsx
+++ src/ViewPage/ViewPage.jsx
@@ -3,15 +3,20 @@ import Flow from "./Flow";
 import "./ViewPage.scss";
 import { Button } from "@mui/material";
 import { useState } from "react";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+
+let editorIsClosed = true;
 
 function ViewPage() {
   let [editorStyle, setEditorStyle] = useState(editorStyleClosed);
-  let editorIsClosed = true;
+  let [buttonText, setButtonText] = useState("Edit");
   let handleClick = () => {
     if (editorIsClosed) {
       setEditorStyle(editorStyleOpen);
+      setButtonText(<KeyboardDoubleArrowLeftIcon />);
     } else {
       setEditorStyle(editorStyleClosed);
+      setButtonText("Edit");
     }
     editorIsClosed = !editorIsClosed;
   };
@@ -19,7 +24,7 @@ function ViewPage() {
     <div className="window-conteiner">
       <div style={editorStyle}>
         <Button variant="contained" style={buttonStyle} onClick={handleClick}>
-          Edit
+          {buttonText}
         </Button>
       </div>
       <div
@@ -34,22 +39,25 @@ function ViewPage() {
 
 let buttonStyle = {
   alignSelf: "center",
-  padding: "3em 1em 3em 1em",
+  padding: "1.5em 1em 1.5em 1em",
 };
 
 let editorStyleOpen = {
   height: "90vh",
-  width: "40vh",
-  backgroundColor: "black",
+  width: "80vh",
   display: "flex",
   flexDirection: "row-reverse",
+  marginRight: "2vh",
+  backgroundColor: "white",
+  border: "3px solid #f8fafb",
+  boxShadow: "0px 8px 24px rgba(149, 157, 165, 0.5)",
 };
 
 let editorStyleClosed = {
   height: "90vh",
-  backgroundColor: "black",
   display: "flex",
   flexDirection: "row-reverse",
+  marginRight: "2vh",
 };
 
 export default ViewPage;
