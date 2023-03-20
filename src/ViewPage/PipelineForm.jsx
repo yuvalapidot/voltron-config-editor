dc902c8f08e2a02ad7b775f440944be4b4afd714
Mor Levi <morlevi0666@gmail.com>
2023-03-20 19:54:49 +0200

    Add documentation

--- src/ViewPage/PipelineForm.jsx
+++ src/ViewPage/PipelineForm.jsx
@@ -3,6 +3,8 @@ import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 import { Grid } from "@mui/material";
 
-function PipelineForm(props) {}
+function PipelineForm(props) {
+    // ToDo
+}
 
 export default PipelineForm;
