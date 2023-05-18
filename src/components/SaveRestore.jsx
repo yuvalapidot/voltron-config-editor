e9ac63f2bb704066a9bc1ac0c7f0883983fd2f4b
mor0666 <morlevi0666@gmail.com>
2023-05-19 02:22:28 +0300

    Change user notification SaveRestore

--- src/components/SaveRestore.jsx
+++ src/components/SaveRestore.jsx
@@ -1,80 +1,94 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from "react";
 import ReactFlow, {
   ReactFlowProvider,
   useReactFlow,
   Controls,
-  ControlButton
-} from 'reactflow';
+  ControlButton,
+} from "reactflow";
 import SaveIcon from "@mui/icons-material/Save";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
-import 'reactflow/dist/style.css';
+import "reactflow/dist/style.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Define the onSave and onRestore event handlers to save and restore the graph state to/from the local storage
 const SaveRestore = (props) => {
-    const [rfInstance, setRfInstance] = useState(null);
-    const { setViewport } = useReactFlow();
-    console.log("props in SaveRestore:")
-    console.log(props)
-    console.log("typeof setNodes: " + typeof props.setNodes);
-    const flowKey = 'example-flow';
-    const onSave = useCallback(() => {
-      console.log('In save')
-      console.log("rfInstance: " + rfInstance)
-      if (rfInstance) {
-          const flow = rfInstance.toObject();
-          console.log('In if of save')
-          console.log("flow: \n" + flow)
-          localStorage.setItem(flowKey, JSON.stringify(flow));
+  const [rfInstance, setRfInstance] = useState(null);
+  const { setViewport } = useReactFlow();
+  const flowKey = "example-flow";
+
+  //Save Function
+  const onSave = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      localStorage.setItem(flowKey, JSON.stringify(flow));
+      toast.success("Saved Successfully", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 5000,
+      });
+    } else {
+      toast.error("Save Failed.", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 5000,
+      });
+    }
+  }, [rfInstance]);
+
+  //Restore Function
+  const onRestore = useCallback(() => {
+    const restoreFlow = async () => {
+      const flow = JSON.parse(localStorage.getItem(flowKey));
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        props.setNodes(flow.nodes || []);
+        props.setEdges(flow.edges || []);
+        setViewport({ x, y, zoom });
+        toast.success("Restored Successfully", {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 5000,
+        });
+      } else {
+        toast.error("Restore Failed. Nothing in localStorage", {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 5000,
+        });
       }
-      }, [rfInstance]);
+    };
 
-    const onRestore = useCallback(() => {
-        const restoreFlow = async () => {
-            const flow = JSON.parse(localStorage.getItem(flowKey));
-            console.log('In restore')
-            console.log("flow: \n" + flow)
-            if (flow) {
-            const { x = 0, y = 0, zoom = 1 } = flow.viewport;
-            console.log('In if of restore')
-            console.log("typeof setNodes: " + typeof props.setNodes);
-            props.setNodes(flow.nodes || []);
-            props.setEdges(flow.edges || []);
-            setViewport({ x, y, zoom });
-            }
-        };
+    restoreFlow();
+  }, [props, setViewport]);
 
-        restoreFlow();
-    }, [props.setNodes, setViewport]);
+  // useEffect(() => {
+  //   onSave();
+  // }, [props.nodes, props.edges, onSave]);
 
-    return (
-        <div>
-        <Controls className="save__controls">
-          <ControlButton onClick={onSave} title='Save to local memory'>
-            <div><SaveIcon/></div>
-          </ControlButton>
-          <ControlButton onClick={onRestore} title='Restore from local memory'>
-            <div><FileUploadIcon/></div>
-          </ControlButton>
-        </Controls>
-        <ReactFlow
-          nodes={props.nodes}
-          edges={props.edges}
-          onNodesChange={props.onNodesChange}
-          onEdgesChange={props.onEdgesChange}
-          onConnect={props.onConnect}
-          onInit={setRfInstance}
-        >
-        </ReactFlow>
-        </div>
-      );
-}
+  return (
+    <div>
+      <Controls className="save__controls">
+        <ControlButton onClick={onSave} title="Save to local memory">
+          <div>
+            <SaveIcon />
+          </div>
+        </ControlButton>
+        <ControlButton onClick={onRestore} title="Restore from local memory">
+          <div>
+            <FileUploadIcon />
+          </div>
+        </ControlButton>
+      </Controls>
+      <ReactFlow
+        nodes={props.nodes}
+        edges={props.edges}
+        onNodesChange={props.onNodesChange}
+        onEdgesChange={props.onEdgesChange}
+        onConnect={props.onConnect}
+        onInit={setRfInstance}
+      ></ReactFlow>
+    </div>
+  );
+};
 
-// export default () => (
-//     <ReactFlowProvider>
-//       <SaveRestore {...props} />
-//     </ReactFlowProvider>
-//   );
-
+//Use ReactFlowProvider as a wrapper to use other React Flow libraries
 function SaveRestoreWithProvider(props) {
   return (
     <ReactFlowProvider>
